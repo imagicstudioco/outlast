5906f1d0171f7606bf60506879057c482d4e8a4e
temitopeohassan <temitope.hassan.2@gmail.com>
2025-06-16 10:56:28 +0100

    Mini App Homepage edit

--- mini-app/app/components/Blank.tsx
+++ mini-app/app/components/Blank.tsx
@@ -1,14 +1,4 @@
-"use client";
 
-import React, { useState, useEffect } from "react";
-import { useAccount, useWalletClient, usePublicClient, useConnect } from "wagmi";
-import { parseUnits, formatUnits } from "viem";
-import { Button } from "./Button";
-import { Card } from "./Card";
-import { API_BASE_URL } from '../config';
-import { injected } from 'wagmi/connectors';
-import { Icon } from "./Icon";
-import { sdk } from '@farcaster/frame-sdk'
 
 
 
